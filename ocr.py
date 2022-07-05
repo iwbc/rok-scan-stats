ba29c171f08e4eb43bb39989c539e650248f3c58
iwbc <1209760+iwbc@users.noreply.github.com>
2022-07-05 21:24:10 +0900

    過去最大戦力のOCR機能追加

--- ocr.py
+++ ocr.py
@@ -53,6 +53,9 @@ KILL_POINT_COEFFICIENTS = (0.2, 2, 4, 10, 20)
 # 戦力の切り抜き範囲
 POWER_CROP_RANGE = (809, 141, 1009, 173)
 
+# 過去最大戦力の切り抜き範囲
+HIGHEST_POWER_CROP_RANGE = (1103, 266, 1303, 296)
+
 # 戦死数の切り抜き範囲
 DEAD_CROP_RANGE = (1103, 450, 1303, 480)
 
@@ -111,6 +114,7 @@ def main():
                 "ID",
                 "Alliance",
                 "Power",
+                "Highest Power",
                 "T1 Kills",
                 "T2 Kills",
                 "T3 Kills",
@@ -218,6 +222,21 @@ def ocr_images(rank: str, name: str):
             [(f"{rank}-power.png", power_img)],
         )
 
+    # 過去最大戦力
+    hpower_img = correct_image(img_b, HIGHEST_POWER_CROP_RANGE, brightness=1.3, contrast=1.8)
+    hpower = ocr_image(hpower_img)
+    hpower = hpower.replace(",", "")
+    if hpower == "":
+        err(
+            f"{rank}位 - {name}: 「過去最大戦力」の読み取りに失敗しました。 -> {rank}-hpower.png",
+            [(f"{rank}-dead.png", hpower_img)],
+        )
+    elif int(power) > int(hpower):
+        err(
+            f"{rank}位 - {name}: 「戦力」または「過去最大戦力」を正しく読み取りできなかった可能性があります。OCRの結果は戦力「{power}」、過去最大戦力「{hpower}」です。 -> {rank}-power.png, {rank}-hpower.png",
+            [(f"{rank}-power.png", power_img), (f"{rank}-hpower.png", hpower_img)],
+        )
+
     # 戦死
     dead_img = correct_image(img_b, DEAD_CROP_RANGE, brightness=1.3, contrast=1.8)
     dead = ocr_image(dead_img)
@@ -239,7 +258,7 @@ def ocr_images(rank: str, name: str):
         )
 
     print(
-        f"{rank} {name} {id} {alliance} {power} {kills[0]} {kills[1]} {kills[2]} {kills[3]} {kills[4]} {dead} {rss}"
+        f"{rank} {name} {id} {alliance} {power} {hpower} {kills[0]} {kills[1]} {kills[2]} {kills[3]} {kills[4]} {dead} {rss}"
     )
 
     return (
@@ -248,6 +267,7 @@ def ocr_images(rank: str, name: str):
         id,
         alliance,
         power,
+        hpower,
         kills[0],
         kills[1],
         kills[2],
